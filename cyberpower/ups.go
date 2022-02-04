c1a29d35dd06265cbfc7ac86f4e92cbe862e9c5f
Jason Ross <jaross@redhat.com>
2022-02-04 13:05:36 -0600

    fix runtime_regex

--- cyberpower/ups.go
+++ cyberpower/ups.go
@@ -41,7 +41,7 @@ type BATTERY_POWER struct {
 }
 
 var ups_path = "/status_update.html"
-var runtime_regex = regexp.MustCompile(`^([0-5]+)min.`)
+var runtime_regex = regexp.MustCompile(`^([0-9]+)min.`)
 var temperature_regex = regexp.MustCompile(`^([0-9]+)°C([0-9]+)°F`)
 
 func (u *UPS) update() {
@@ -119,8 +119,8 @@ func process_ups_group(group *html.Node, label_group *html.Node, u *UPS) {
 					}
 				case "Remaining Runtime":
 					rs := runtime_regex.FindStringSubmatch(curr_item.FirstChild.Data)
-					if len(rs) != 2 {
-						log.Printf("Unable to parse Tempurature for %s", label_group.FirstChild.Data)
+					if len(rs) < 2 {
+						log.Printf("Unable to parse Remaining Runtime for %s", label_group.FirstChild.Data)
 						break
 					}
 					rr, err := strconv.Atoi(rs[1])
